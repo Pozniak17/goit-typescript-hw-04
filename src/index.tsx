9146f8768ca59abcd6bdbe13f260226573ba0791
Yevhenii Pozniak <jekamanu@gmail.com>
2024-11-04 06:43:45 +0200

    initial files

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./components/App";
-import { User } from "./components/App/App.types";
-
-const user: User = {
-  id: 1,
-  name: "John",
-  email: "mail@example.com",
-};
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App user={user} />
+    <App />
   </React.StrictMode>
 );
